e8c2cc9e752f7d158d5007bc0cfc295a81b6dcb5
UjeNeTORT <tikhonovty@gmail.com>
2026-03-14 13:40:23 +0300

    [01_fp16] fix fp16add nan, inf, ftz, daz corner cases

--- problems/2026/01_fp16/fp16add.v
+++ problems/2026/01_fp16/fp16add.v
@@ -39,36 +39,62 @@ always @(i_a, i_b) begin
     sgn_2 = i_b[15];
   end
 
-  exp_diff = exp_1 - exp_2;
+  // denormal as zero
+  if (exp_1 == 0) mw_1[11:0] = 0;
+  if (exp_2 == 0) mw_2[11:0] = 0;
 
-  mw_2 = mw_2 >> exp_diff;
+  if (exp_1 == 5'b11111) begin
+    // nan/inf case
+    o_res = {sgn_1, exp_1, mw_1};
+  end else begin
 
-  mw_sum = sgn_1 == 0 ?  $signed(mw_1)
-                      : -$signed(mw_1);
-  mw_sum = sgn_2 == 0 ? $signed(mw_sum) + $signed(mw_2)
-                      : $signed(mw_sum) - $signed(mw_2);
+    exp_diff = exp_1 - exp_2;
 
-  sgn_res = mw_sum[14];
-  mw_sum_abs = sgn_res == 1 ? -$signed(mw_sum)
-                            : mw_sum;
+    mw_2 = mw_2 >> exp_diff;
 
-  // find leading one
-  flo_found = 0;
-  m_shift = 0;
-  for (integer i = 0; i < 13; i++) begin
-    // mw_sum_abs[14] == 0 because the number is positive
-    if (mw_sum_abs[13 - i] == 1 && !flo_found) begin
-      flo_found = 1;
-      m_shift = i;
+    mw_sum = sgn_1 == 0 ?  $signed(mw_1)
+                        : -$signed(mw_1);
+    mw_sum = sgn_2 == 0 ? $signed(mw_sum) + $signed(mw_2)
+                        : $signed(mw_sum) - $signed(mw_2);
+
+    sgn_res = mw_sum[14];
+    mw_sum_abs = sgn_res == 1 ? -$signed(mw_sum)
+                              : mw_sum;
+
+    // find leading one
+    flo_found = 0;
+    m_shift = 0;
+    for (integer i = 0; i < 13; i++) begin
+      // mw_sum_abs[14] == 0 because the number is positive
+      if (mw_sum_abs[13 - i] == 1 && !flo_found) begin
+        flo_found = 1;
+        m_shift = i;
+      end
     end
+
+    if (flo_found != 0) begin
+      // 01x.xxxxxxxxxxxx
+      //   +---------+
+      mw_sum_abs_norm = mw_sum_abs << m_shift;
+      m_res_norm = mw_sum_abs_norm[12:3]; // round towards zero
+
+      if (exp_1 + 1 > m_shift) begin
+        exp_res = exp_1 + 1 - m_shift;
+      end else begin
+        exp_res = 0; // exp < 0 -> denormal case
+      end
+
+      // flush denormals to zero
+      if (exp_res == 0) begin
+        m_res_norm = 0;
+      end
+    end else begin
+      // no leading one => result is 0
+      exp_res = 0;
+      m_res_norm = 0;
+      sgn_res = 0;
+    end
+    o_res = {sgn_res, exp_res, m_res_norm};
   end
-
-  // 01x.xxxxxxxxxxxx
-  //   +---------+
-  mw_sum_abs_norm = mw_sum_abs << m_shift;
-  m_res_norm = mw_sum_abs_norm[12:3]; // round towards zero
-  exp_res = exp_1 + 1 - m_shift; // + 1 to handle 1.0 + 1.1 = 11.1 case (msb)
-
-  o_res = {sgn_res, exp_res, m_res_norm};
 end
 endmodule
